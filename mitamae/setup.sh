172cd01ed7af5a2b38d6042f19960685c371e7a2
motoki-shun <shuuuuuny+github@gmail.com>
2020-12-06 13:20:04 +0900

    fix setup

--- mitamae/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-mitamae_version="1.11.7"
-curl -sL -o /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-linux
-chmod +x /usr/local/bin/mitamae
-mitamae version
