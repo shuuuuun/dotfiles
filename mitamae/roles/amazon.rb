a28ffd0bfbf738a315f0cefb3ef7c61943dd71ee
motoki-shun <shuuuuuny+github@gmail.com>
2020-12-06 11:28:11 +0900

    tmux 3.1c

--- mitamae/roles/amazon.rb
+++ mitamae/roles/amazon.rb
@@ -1,7 +1,20 @@
+node.reverse_merge!(
+  tmux: {
+    version: "3.1c",
+  },
+  libevent: {
+    version: "2.1.12",
+  },
+  ncurses: {
+    version: "6.2",
+  }
+)
+
 include_cookbook 'git'
 include_cookbook 'vim'
 include_cookbook 'tig'
-include_cookbook 'tmux'
+# include_cookbook 'tmux'
+include_recipe 'tmux'
 # include_cookbook 'anyenv'
 # include_cookbook 'docker'
 # include_cookbook 'mosh'
