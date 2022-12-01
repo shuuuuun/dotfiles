da8ff68dc91dbbe9173fb61a3c889ca2caf734db
motoki-shun <shuuuuun@users.noreply.github.com>
2022-12-01 07:25:44 +0000

    mitamae: add ubuntu role

---
+++ mitamae/roles/ubuntu.rb
@@ -0,0 +1,28 @@
+node.reverse_merge!(
+  tmux: {
+    version: "3.1c",
+    prefix: "/usr/local",
+    profile: "/etc/profile",
+  },
+  libevent: {
+    version: "2.1.12",
+  },
+  ncurses: {
+    version: "6.2",
+  }
+)
+
+include_cookbook 'git'
+include_cookbook 'vim'
+include_cookbook 'tig'
+
+include_recipe 'tmux'
+include_recipe 'docker::install'
+# include_cookbook 'anyenv'
+
+# include_cookbook 'mosh'
+# include_cookbook 'ghq'
+# include_cookbook 'zsh'
+# include_cookbook 'peco'
+# include_cookbook 'local-accounts'
+# include_recipe 'ssh-agent'
